5dcadc53e15b93a63108c0a596b76c2736f54ed9
catgonza <67137308+catgonza@users.noreply.github.com>
2020-10-27 20:53:17 +1100

    first commit

---
+++ script.js
@@ -0,0 +1,48 @@
+$("#pop").on("click", function() {
+    $('#imagepreview').attr('src', $('#imageresource').attr('src')); // here asign the image to the modal when the user click the enlarge link
+    $('#imagemodal').modal('show'); // imagemodal is the id attribute assigned to the bootstrap modal, then i use the show function
+ });
+ $("#pop1").on("click", function() {
+    $('#imagepreview1').attr('src', $('#imageresource1').attr('src')); 
+    $('#imagemodal1').modal('show'); 
+ });
+ $("#pop2").on("click", function() {
+    $('#imagepreview2').attr('src', $('#imageresource2').attr('src')); 
+    $('#imagemodal2').modal('show'); 
+ });
+ $("#pop3").on("click", function() {
+    $('#imagepreview3').attr('src', $('#imageresource3').attr('src')); 
+    $('#imagemodal3').modal('show'); 
+ });
+ $("#pop4").on("click", function() {
+    $('#imagepreview4').attr('src', $('#imageresource4').attr('src')); 
+    $('#imagemodal4').modal('show'); 
+ });
+ $("#pop5").on("click", function() {
+    $('#imagepreview5').attr('src', $('#imageresource5').attr('src')); 
+    $('#imagemodal5').modal('show'); 
+ });
+ $("#pop6").on("click", function() {
+    $('#imagepreview6').attr('src', $('#imageresource6').attr('src')); 
+    $('#imagemodal6').modal('show'); 
+ });
+ $("#pop7").on("click", function() {
+    $('#imagepreview7').attr('src', $('#imageresource7').attr('src')); 
+    $('#imagemodal7').modal('show'); 
+ });
+ $("#pop8").on("click", function() {
+    $('#imagepreview8').attr('src', $('#imageresource8').attr('src')); 
+    $('#imagemodal8').modal('show'); 
+ });
+ $("#pop9").on("click", function() {
+    $('#imagepreview9').attr('src', $('#imageresource9').attr('src')); 
+    $('#imagemodal9').modal('show'); 
+ });
+ $("#pop10").on("click", function() {
+    $('#imagepreview10').attr('src', $('#imageresource10').attr('src')); 
+    $('#imagemodal10').modal('show'); 
+ });
+ $("#pop11").on("click", function() {
+    $('#imagepreview11').attr('src', $('#imageresource11').attr('src')); 
+    $('#imagemodal11').modal('show'); 
+ });
